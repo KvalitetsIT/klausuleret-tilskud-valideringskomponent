c57503a9ae28d2502ee773ea3e6e72a1f059720b
Henrik Eidnes <hen@kvalitetsit.dk>
2025-06-23 15:54:55 +0200

    Refactor
    Set up shared instance of the API for testing in the IntegrationTest class. This ensures only one instance is running.

--- integrationtest/src/test/java/dk/kvalitetsit/hello/integrationtest/HelloIT.java
+++ integrationtest/src/test/java/dk/kvalitetsit/hello/integrationtest/HelloIT.java
@@ -1,30 +1,23 @@
 package dk.kvalitetsit.hello.integrationtest;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.JSON;
-import org.openapitools.client.api.KithugsApi;
 import org.openapitools.client.model.DetailedError;
 
+import static dk.kvalitetsit.hello.integrationtest.IntegrationTest.api;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openapitools.client.model.HelloRequest;
 
 class HelloIT {
-
     static final String input = "Some Name";
-    static final KithugsApi helloApi = setupTest(input);
 
-    static KithugsApi setupTest(String initialData) {
-        KithugsApi api = IntegrationTest.startService();
-        post(api, initialData);
-        return api;
-    }
-
-    static void post(KithugsApi api, String input) {
-        var r = new HelloRequest(); r.setName(input);
+    @BeforeAll
+    static void setup() {
         try {
-            api.v1HelloPost(r);
+            api.v1HelloPost(new HelloRequest().name(input));
         } catch (ApiException e) {
             throw new RuntimeException(e);
         }
@@ -32,13 +25,13 @@ class HelloIT {
 
     @Test
     void testCallServiceWithName() throws ApiException {
-        var result = helloApi.v1HelloGet(input);
+        var result = api.v1HelloGet(input);
 
         assertNotNull(result);
         assertEquals(1, result.size());
         boolean containsSomeName = result.stream()
                 .anyMatch(dbEntry -> input.equals(dbEntry.getName()));
-        assert(containsSomeName);
+        assert (containsSomeName);
     }
 
     @Test
@@ -46,7 +39,7 @@ class HelloIT {
         //Test that calling the get method with a name that is not in the db returns an empty list
         var input = "notindb";
 
-        var result = helloApi.v1HelloGet(input);
+        var result = api.v1HelloGet(input);
 
         assertNotNull(result);
         assertEquals(0, result.size());
@@ -56,20 +49,20 @@ class HelloIT {
     void testCallServiceWithoutName() throws ApiException {
         String input = null;
 
-        var result = helloApi.v1HelloGet(input);
+        var result = api.v1HelloGet(input);
 
         assertNotNull(result);
         assertEquals(1, result.size());
         boolean containsSomeName = result.stream()
                 .anyMatch(dbEntry -> HelloIT.input.equals(dbEntry.getName()));
-        assert(containsSomeName);
+        assert (containsSomeName);
     }
 
     @Test
     void testCallPostService() throws ApiException {
         var request = new HelloRequest().name("John Dow");
 
-        var postResult = helloApi.v1HelloPost(request);
+        var postResult = api.v1HelloPost(request);
         assertNotNull(postResult);
         assertEquals(request.getName(), postResult.getName());
         assertNull(postResult.getiCanBeNull());
@@ -80,7 +73,7 @@ class HelloIT {
     void testCallGetServiceNameTooLong() {
         var input = "John Doe Is Too Long";
 
-        var thrownException = assertThrows(ApiException.class, () -> helloApi.v1HelloGet(input));
+        var thrownException = assertThrows(ApiException.class, () -> api.v1HelloGet(input));
         DetailedError detailedError = JSON.deserialize(thrownException.getResponseBody(), DetailedError.class);
         assertEquals("Bad Request", detailedError.getError());
         assertEquals("/v1/hello", detailedError.getPath());
@@ -94,7 +87,7 @@ class HelloIT {
     void testCallPostServiceNameTooLong() {
         var request = new HelloRequest().name("John Doe Is Too Long");
 
-        var thrownException = assertThrows(ApiException.class, () -> helloApi.v1HelloPost(request));
+        var thrownException = assertThrows(ApiException.class, () -> api.v1HelloPost(request));
         DetailedError detailedError = JSON.deserialize(thrownException.getResponseBody(), DetailedError.class);
         assertEquals("Bad Request", detailedError.getError());
         assertEquals("/v1/hello", detailedError.getPath());
@@ -108,7 +101,7 @@ class HelloIT {
     void testCallServiceNameValidationError() {
         var input = "NOT_VALID";
 
-        var thrownException = assertThrows(ApiException.class, () -> helloApi.v1HelloGet(input));
+        var thrownException = assertThrows(ApiException.class, () -> api.v1HelloGet(input));
         assertEquals(400, thrownException.getCode());
 
         DetailedError detailedError = JSON.deserialize(thrownException.getResponseBody(), DetailedError.class);

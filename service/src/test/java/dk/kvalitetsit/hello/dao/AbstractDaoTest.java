5d8347bdda1fdf5309323a267ab2386dc060295e
Thomas Lange <tsl@kvalitetsit.dk>
2025-06-27 12:10:39 +0200

    Stop spring from performing db migrations. Move to flyway container

--- service/src/test/java/dk/kvalitetsit/hello/dao/AbstractDaoTest.java
+++ service/src/test/java/dk/kvalitetsit/hello/dao/AbstractDaoTest.java
@@ -1,7 +1,7 @@
 package dk.kvalitetsit.hello.dao;
 
-import dk.kvalitetsit.hello.configuration.TestConfiguration;
 import dk.kvalitetsit.hello.configuration.DatabaseConfiguration;
+import dk.kvalitetsit.hello.configuration.TestConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.annotation.PropertySource;
@@ -14,14 +14,14 @@ import org.testcontainers.containers.MariaDBContainer;
 @ExtendWith(SpringExtension.class)
 @PropertySource("test.properties")
 @ContextConfiguration(
-        classes = { TestConfiguration.class, DatabaseConfiguration.class},
+        classes = {TestConfiguration.class, DatabaseConfiguration.class},
         loader = AnnotationConfigContextLoader.class)
 @Transactional
 abstract public class AbstractDaoTest {
     private static Object initialized = null;
 
     @BeforeAll
-   public static void setupMariadbJdbcUrl() {
+    public static void setupMariadbJdbcUrl() {
         if (initialized == null) {
             var username = "hellouser";
             var password = "secret1234";

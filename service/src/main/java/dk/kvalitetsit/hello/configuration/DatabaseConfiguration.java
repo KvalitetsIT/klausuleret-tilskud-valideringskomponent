3c2dd16c8dc8164077413afa968d8202e4c2b92f
Thomas Lange <tsl@kvalitetsit.dk>
2025-06-27 09:08:52 +0200

    Add configuration for db connection pool

--- service/src/main/java/dk/kvalitetsit/hello/configuration/DatabaseConfiguration.java
+++ service/src/main/java/dk/kvalitetsit/hello/configuration/DatabaseConfiguration.java
@@ -8,17 +8,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Configuration
 @EnableTransactionManagement
 public class DatabaseConfiguration {
 
     @Bean
-    public DataSource dataSource(@Value("${jdbc.url}") String jdbcUrl, @Value("${jdbc.user}") String jdbcUser, @Value("${jdbc.pass}") String jdbcPass) {
+    public DataSource dataSource(@Value("${jdbc.url}") String jdbcUrl,
+                                 @Value("${jdbc.user}") String jdbcUser,
+                                 @Value("${jdbc.pass}") String jdbcPass,
+                                 @Value("${jdbc.connection.test.query:#{null}}") String testQuery,
+                                 @Value("${jdbc.connection.max.age:1800000}") Long maxConnectionAge,
+                                 @Value("${jdbc.connection.max.idle.time:#{null}}") Long maxConnectionIdleTime) {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(jdbcUrl);
         hikariConfig.setUsername(jdbcUser);
         hikariConfig.setPassword(jdbcPass);
+        Optional.ofNullable(testQuery).ifPresent(hikariConfig::setConnectionTestQuery);
+        hikariConfig.setMaxLifetime(maxConnectionAge);
+        Optional.ofNullable(maxConnectionIdleTime).ifPresent(hikariConfig::setIdleTimeout);
+
         return new HikariDataSource(hikariConfig);
     }
 }

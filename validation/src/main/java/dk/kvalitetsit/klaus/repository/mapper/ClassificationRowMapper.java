72ef5d99f703663f35edf3b4b5a010d103f8baba
Jonas <jqh@kvalitetsit.dk>
2025-07-23 13:21:32 +0200

    added mapping

---
+++ validation/src/main/java/dk/kvalitetsit/klaus/repository/mapper/ClassificationRowMapper.java
@@ -0,0 +1,23 @@
+package dk.kvalitetsit.klaus.repository.mapper;
+
+import dk.kvalitetsit.klaus.repository.entity.ClauseEntity;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClassificationRowMapper implements RowMapper<ClauseEntity> {
+
+    @Override
+    public ClauseEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new ClauseEntity(
+                rs.getString("Kode"),
+                rs.getTimestamp("ValidTo").toLocalDateTime(),
+                rs.getLong("KlausuleringPID"),
+                rs.getString("KortTekst"),
+                rs.getTimestamp("LastReplicated"),
+                rs.getString("Tekst"),
+                rs.getTimestamp("ValidFrom").toLocalDateTime()
+        );
+    }
+}
+

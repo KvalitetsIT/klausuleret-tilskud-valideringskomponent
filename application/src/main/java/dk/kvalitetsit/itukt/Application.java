f4f8e3a70225c2bf1bf76cd49416caa0f2467469
Thomas Lange <tsl@kvalitetsit.dk>
2025-08-13 16:33:53 +0200

    Get rid of EnableConfigurationProperties annotations

--- application/src/main/java/dk/kvalitetsit/itukt/Application.java
+++ application/src/main/java/dk/kvalitetsit/itukt/Application.java
@@ -3,8 +3,10 @@ package dk.kvalitetsit.itukt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("dk.kvalitetsit.itukt")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

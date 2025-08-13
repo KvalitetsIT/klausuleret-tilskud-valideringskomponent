4ecc4d6b9911c88068922d45c548c7e1e4153101
Thomas Lange <tsl@kvalitetsit.dk>
2025-08-13 13:06:38 +0200

    Revert "Move app db configuration to management module"
    This reverts commit 5eef38481e311406a12a55f24c5a090c67eba58c.

--- common/src/main/java/dk/kvalitetsit/itukt/common/configuration/CommonBeanRegistration.java
+++ common/src/main/java/dk/kvalitetsit/itukt/common/configuration/CommonBeanRegistration.java
@@ -1,18 +1,46 @@
 package dk.kvalitetsit.itukt.common.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
 @EnableTransactionManagement
+@EnableConfigurationProperties(CommonConfiguration.class)
 public class CommonBeanRegistration {
+
+    private final CommonConfiguration configuration;
+
+    public CommonBeanRegistration(CommonConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Bean("appDataSource")
+    public DataSource appDataSource() {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(configuration.jdbc().url());
+        hikariConfig.setUsername(configuration.jdbc().username());
+        hikariConfig.setPassword(configuration.jdbc().password());
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public PlatformTransactionManager clauseTransactionManager(@Qualifier("appDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
     @Bean
     public CorsFilter corsFilter(@Value("#{'${allowed_origins:http://localhost}'}") List<String> allowedOrigins) {
--- management/src/main/java/dk/kvalitetsit/itukt/management/configuration/ManagementConfiguration.java
+++ management/src/main/java/dk/kvalitetsit/itukt/management/configuration/ManagementConfiguration.java
@@ -1,11 +1,9 @@
 package dk.kvalitetsit.itukt.management.configuration;
 
-import dk.kvalitetsit.itukt.common.model.Datasource;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app.management")
-public record ManagementConfiguration(@NotNull Datasource jdbc) {
+public record ManagementConfiguration() {
 }

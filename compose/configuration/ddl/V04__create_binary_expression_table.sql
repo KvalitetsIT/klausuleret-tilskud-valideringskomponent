1b806c3f7fbda485c2fff859ccdf6a06424fd010
Jonas <jqh@kvalitetsit.dk>
2025-08-14 14:02:44 +0200

    added a few more unique constraints

--- compose/configuration/ddl/V04__create_binary_expression_table.sql
+++ compose/configuration/ddl/V04__create_binary_expression_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE binary_expression (
     expression_id INT PRIMARY KEY,
-    left_id INT NOT NULL,
+    left_id INT NOT NULL UNIQUE,
     operator VARCHAR(255) NOT NULL,
-    right_id INT NOT NULL,
+    right_id INT NOT NULL UNIQUE,
     FOREIGN KEY (expression_id) REFERENCES expression(id) ON DELETE CASCADE,
     FOREIGN KEY (left_id) REFERENCES expression(id),
     FOREIGN KEY (right_id) REFERENCES expression(id)
--- compose/configuration/ddl/V05_create_parenthesized_expression_table.sql
+++ compose/configuration/ddl/V05_create_parenthesized_expression_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE parenthesized_expression (
     expression_id INT PRIMARY KEY,
-    inner_id INT NOT NULL,
+    inner_id INT NOT NULL UNIQUE,
     FOREIGN KEY (expression_id) REFERENCES expression(id) ON DELETE CASCADE,
     FOREIGN KEY (inner_id) REFERENCES expression(id)
 );

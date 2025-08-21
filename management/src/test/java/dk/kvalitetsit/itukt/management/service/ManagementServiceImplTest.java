e56160388c06e11297c336e9f8045f5cadb97595
Jonas <jqh@kvalitetsit.dk>
2025-08-21 11:38:42 +0200

    added test for read and read_all

--- management/src/test/java/dk/kvalitetsit/itukt/management/service/ManagementServiceImplTest.java
+++ management/src/test/java/dk/kvalitetsit/itukt/management/service/ManagementServiceImplTest.java
@@ -4,6 +4,7 @@ package dk.kvalitetsit.itukt.management.service;
 import dk.kvalitetsit.itukt.common.model.Clause;
 import dk.kvalitetsit.itukt.common.model.Expression;
 import dk.kvalitetsit.itukt.common.model.Operator;
+import dk.kvalitetsit.itukt.management.MockFactory;
 import dk.kvalitetsit.itukt.management.repository.ClauseRepositoryAdaptor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,7 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,5 +36,22 @@ class ManagementServiceImplTest {
         var result = service.create(input);
         assertNotNull(result);
     }
+
+    @Test
+    void testRead() {
+        var model = MockFactory.clauseModel;
+        Mockito.when(dao.read(model.uuid().get())).thenReturn(Optional.of(model));
+        var result = service.read(model.uuid().get());
+        assertEquals(Optional.of(model), result);
+    }
+
+    @Test
+    void testReadAll() {
+        var model = MockFactory.clauseModel;
+        Mockito.when(dao.readAll()).thenReturn(List.of(model));
+        var result = service.readAll();
+        assertEquals(List.of(model), result);
+    }
+
 
 }

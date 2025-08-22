613cc7c2b1e94e76fefcc35e1dd1b278844a77cd
Jonas <jqh@kvalitetsit.dk>
2025-08-22 10:06:46 +0200

    added validationServiceAdaptorTest

--- validation/src/main/java/dk/kvalitetsit/itukt/validation/boundary/mapping/ValidationDataContextMapper.java
+++ validation/src/main/java/dk/kvalitetsit/itukt/validation/boundary/mapping/ValidationDataContextMapper.java
@@ -5,16 +5,22 @@ import dk.kvalitetsit.itukt.validation.service.model.DataContext;
 import org.openapitools.model.ExistingDrugMedication;
 import org.openapitools.model.ValidationRequest;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ValidationDataContextMapper implements Mapper<ValidationRequest, DataContext> {
     @Override
     public DataContext map(ValidationRequest entry) {
-        return new DataContext(Map.of(
-                "ALDER", List.of(entry.getAge().toString()),
-                "ATC", entry.getExistingDrugMedications().orElse(List.of()).stream().map(ExistingDrugMedication::getAtcCode).toList()
-        ));
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        map.put("ALDER", List.of(entry.getAge().toString()));
+
+        entry.getExistingDrugMedications().ifPresent(x -> {
+            if (!x.isEmpty()) map.put("ATC", x.stream().map(ExistingDrugMedication::getAtcCode).toList());
+        });
+
+        return new DataContext(map);
     }
 
 }

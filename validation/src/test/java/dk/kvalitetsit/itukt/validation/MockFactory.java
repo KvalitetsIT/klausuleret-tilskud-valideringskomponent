db14dc9080b98a5934cb119b01625cb231eab0da
Jonas <jqh@kvalitetsit.dk>
2025-08-22 12:01:16 +0200

    added test for eval

--- validation/src/test/java/dk/kvalitetsit/itukt/validation/MockFactory.java
+++ validation/src/test/java/dk/kvalitetsit/itukt/validation/MockFactory.java
@@ -1,20 +1,43 @@
 package dk.kvalitetsit.itukt.validation;
 
+import dk.kvalitetsit.itukt.validation.service.model.DataContext;
 import org.openapitools.model.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 public class MockFactory {
 
-    public static final String dsl = """
-            Klausul CHOL:
-            (ATC = C10BA03)
-            eller
-            (ATC i C10BA02, C10BA05) og (ALDER >= 13)
-            """;
 
+    public static final String dsl = "Klausul CHOL: (ATC = C10BA03) eller (ATC i C10BA02, C10BA05) og (ALDER >= 13)";
 
-    public static final Expression clause = new BinaryExpression()
+    public static final dk.kvalitetsit.itukt.common.model.Expression expressionModel = new dk.kvalitetsit.itukt.common.model.Expression.BinaryExpression(
+            new dk.kvalitetsit.itukt.common.model.Expression.Condition(
+                    "ATC",
+                    dk.kvalitetsit.itukt.common.model.Operator.EQUAL,
+                    List.of("C10BA03")
+            ),
+            dk.kvalitetsit.itukt.common.model.Expression.BinaryExpression.BinaryOperator.OR,
+            new dk.kvalitetsit.itukt.common.model.Expression.BinaryExpression(
+                    new dk.kvalitetsit.itukt.common.model.Expression.Condition(
+                            "ATC",
+                            dk.kvalitetsit.itukt.common.model.Operator.IN,
+                            List.of("C10BA02", "C10BA05")
+                    ),
+                    dk.kvalitetsit.itukt.common.model.Expression.BinaryExpression.BinaryOperator.AND,
+                    new dk.kvalitetsit.itukt.common.model.Expression.Condition(
+                            "ALDER",
+                            dk.kvalitetsit.itukt.common.model.Operator.GREATER_THAN_OR_EQUAL_TO,
+                            List.of("13")
+                    )
+            )
+    );
+
+    public static final dk.kvalitetsit.itukt.common.model.Clause clauseModel = new dk.kvalitetsit.itukt.common.model.Clause("CHOL", Optional.of(UUID.randomUUID()), expressionModel);
+
+    public static final Expression expressionDto = new BinaryExpression()
             .type("BinaryExpression")
             .operator(BinaryOperator.OR)
             .left(new Condition().type("Condition").field("ATC").operator(Operator.EQUAL).values(List.of("C10BA03")))
@@ -23,4 +46,9 @@ public class MockFactory {
                     .right(new Condition().type("Condition").field("ALDER").operator(Operator.GREATER_THAN_OR_EQUAL_TO).values(List.of("13"))
                     )
             );
+
+    public static DataContext ctx = new DataContext(Map.of(
+            "ALDER", List.of("13"),
+            "ATC", List.of("C10BA02", "C10BA05")
+    ));
 }

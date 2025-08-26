6a245cc98618386367bf52b66882b89629aeefdf
Jonas <jqh@kvalitetsit.dk>
2025-08-26 13:08:08 +0200

    added ValidationDataContextMapperTest

--- validation/src/test/java/dk/kvalitetsit/itukt/validation/service/ValidationServiceAdaptorTest.java
+++ validation/src/test/java/dk/kvalitetsit/itukt/validation/service/ValidationServiceAdaptorTest.java
@@ -1,5 +1,6 @@
 package dk.kvalitetsit.itukt.validation.service;
 
+import dk.kvalitetsit.itukt.validation.MockFactory;
 import dk.kvalitetsit.itukt.validation.boundary.mapping.ValidationDataContextMapper;
 import dk.kvalitetsit.itukt.validation.service.model.DataContext;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,24 +27,29 @@ public class ValidationServiceAdaptorTest {
     @Mock
     private ValidationServiceImpl concreteService;
 
+    @Mock
+    private ValidationDataContextMapper validationDataContextMapper;
+
     @BeforeEach
     void setUp() {
-        adaptor = new ValidationServiceAdaptor(concreteService, new ValidationDataContextMapper());
+        adaptor = new ValidationServiceAdaptor(concreteService, validationDataContextMapper);
     }
 
     @Test
     void testValidate() {
 
         Mockito.when(concreteService.validate(Mockito.any(DataContext.class))).thenReturn(true);
-
         var request = new ValidationRequest().age(20);
+        var ctx = new DataContext(Map.of("ALDER", List.of("20")));
+
+        Mockito.when(validationDataContextMapper.map(request)).thenReturn(ctx);
+
         var result = adaptor.validate(request);
         assertTrue(result);
 
         var captor = ArgumentCaptor.forClass(DataContext.class);
         Mockito.verify(concreteService, Mockito.times(1)).validate(captor.capture());
 
-        var ctx = new DataContext(Map.of("ALDER", List.of("20")));
         assertEquals(ctx, captor.getValue());
     }
 

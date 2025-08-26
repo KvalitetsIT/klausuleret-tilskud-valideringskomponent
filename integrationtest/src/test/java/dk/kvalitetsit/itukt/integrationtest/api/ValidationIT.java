f3bfa44bc697be6b4a56f7140278ccd253245c29
Thomas Lange <tsl@kvalitetsit.dk>
2025-08-26 13:41:50 +0200

    Add integration tests for age validation

--- integrationtest/src/test/java/dk/kvalitetsit/itukt/integrationtest/api/ValidationIT.java
+++ integrationtest/src/test/java/dk/kvalitetsit/itukt/integrationtest/api/ValidationIT.java
@@ -3,44 +3,81 @@ package dk.kvalitetsit.itukt.integrationtest.api;
 import dk.kvalitetsit.itukt.integrationtest.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openapitools.client.api.ManagementApi;
 import org.openapitools.client.api.ValidationApi;
-import org.openapitools.client.model.DslInput;
-import org.openapitools.client.model.ExistingDrugMedication;
-import org.openapitools.client.model.ValidationRequest;
-import org.openapitools.client.model.ValidationSuccess;
-import org.springframework.web.client.RestClientResponseException;
+import org.openapitools.client.model.*;
 
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class ValidationIT extends BaseTest {
 
     private ValidationApi validationApi;
-    private ManagementApi managementApi;
 
     @BeforeEach
     void setup() {
         this.validationApi = new ValidationApi(client);
-        this.managementApi = new ManagementApi(client);
     }
 
     @Test
-    void testPostValidation() {
-        managementApi.call20250801clausesDslPost(new DslInput().dsl("Klausul CHOL: (ATC = C10BA03) eller (ATC i C10BA02, C10BA05) og (ALDER >= 13)"));
-        var existingDrugMedication = new ExistingDrugMedication()
-                .atcCode("C10BA05")
-                .drugIdentifier(123456789L)
-                .formCode("TAB")
-                .routeOfAdministrationCode("OK");
-        var request = new ValidationRequest()
-                .age(18)
+    void call20250801validatePost_WithInputThatMatchesClauseAndValidates_ReturnsSuccess() {
+        long drugId = 1L;// Matches hardcoded value in cache
+        String elementPath = "path";
+        int age = 51;  // Hardcoded clause in cache requires age > 50
+        var request = createValidationRequest(drugId, elementPath, age);
+
+        var response = validationApi.call20250801validatePost(request);
+
+        assertInstanceOf(ValidationSuccess.class, response);
+    }
+
+    @Test
+    void call20250801validatePost_WithInputThatMatchesClauseAndFailsValidation_ReturnsValidationError() {
+        long drugId = 1L;// Matches hardcoded value in cache
+        String elementPath = "path";
+        int age = 50;  // Hardcoded clause in cache requires age > 50
+        var request = createValidationRequest(drugId, elementPath, age);
+
+        var response = validationApi.call20250801validatePost(request);
+
+        assertInstanceOf(ValidationFailed.class, response);
+        var failedResponse = (ValidationFailed) response;
+        assertEquals(1, failedResponse.getValidationErrors().size());
+        var validationError = failedResponse.getValidationErrors().get(0);
+        String expectedClauseCode = "KRINI"; // Hardcoded clause code in stamdata cache
+        assertEquals(expectedClauseCode, validationError.getClauseCode());
+        assertEquals(elementPath, validationError.getElementPath());
+    }
+
+    private ValidationRequest createValidationRequest(long drugId, String elementPath, int age) {
+        Validate validate = createValidateElement(drugId, elementPath);
+        return new ValidationRequest()
+                .age(age)
                 .personIdentifier("1234567890")
-                .addExistingDrugMedicationsItem(existingDrugMedication);
-        try {
-            var response = validationApi.call20250801validatePost(request);
-            assertInstanceOf(ValidationSuccess.class, response);
-        } catch (RestClientResponseException e) {
-            throw new RuntimeException(e);
-        }
+                .addValidateItem(validate);
+    }
+
+    private Validate createValidateElement(long drugId, String path) {
+        NewDrugMedication newDrugMedication = createNewDrugMedication(drugId);
+        return new Validate()
+                .action(Validate.ActionEnum.CREATE_DRUG_MEDICATION)
+                .elementPath(path)
+                .newDrugMedication(newDrugMedication);
+    }
+
+    private NewDrugMedication createNewDrugMedication(long drugId) {
+        return new NewDrugMedication()
+                .drugIdentifier(drugId)
+                .indicationCode("")
+                .createdBy(createActor())
+                .reportedBy(createActor())
+                .createdDateTime(OffsetDateTime.now());
+    }
+
+    private Actor createActor() {
+        return new Actor()
+                .organisationSpeciality("")
+                .specialityCode("");
     }
 }

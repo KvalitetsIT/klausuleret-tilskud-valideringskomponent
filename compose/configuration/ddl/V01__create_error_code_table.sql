f6ba800db2206f14b9f7fe4a3bd94a436f8bb53d
Jonas <jqh@kvalitetsit.dk>
2025-09-04 08:38:15 +0200

    changed clause_uuid to clause_name

--- compose/configuration/ddl/V01__create_error_code_table.sql
+++ compose/configuration/ddl/V01__create_error_code_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE error_code (
     error_code INT PRIMARY KEY,
-    clause_uuid VARCHAR(36) NOT NULL
+    clause_name VARCHAR(255) NOT NULL
 ) AUTO_INCREMENT = 10800
 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 COMMENT='Error codes from 10800 to 10999 (max 200 rows)';

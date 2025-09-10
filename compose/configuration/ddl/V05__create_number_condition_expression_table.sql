d1f7c5153e22ce040e302babf17c56f421f8e04c
Thomas Lange <tsl@kvalitetsit.dk>
2025-09-10 16:43:53 +0200

    Make operator enum in number_condition_expression

--- compose/configuration/ddl/V05__create_number_condition_expression_table.sql
+++ compose/configuration/ddl/V05__create_number_condition_expression_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `number_condition_expression` (
     expression_id INT PRIMARY KEY,
     field VARCHAR(255) NOT NULL,
-    operator VARCHAR(255) NOT NULL,
+    operator ENUM('=','>','<','>=','<=') NOT NULL,
     value INT NOT NULL,
     FOREIGN KEY (expression_id) REFERENCES expression(id) ON DELETE CASCADE
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin

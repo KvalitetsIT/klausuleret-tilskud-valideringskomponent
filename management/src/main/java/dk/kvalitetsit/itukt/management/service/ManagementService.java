c9ecc89fc2f7ced6412117f1ecb6d2ec2f552f19
Thomas Lange <tsl@kvalitetsit.dk>
2025-09-17 14:53:57 +0200

    Remove CRUD interface + accept name and expression only when creating clause

--- management/src/main/java/dk/kvalitetsit/itukt/management/service/ManagementService.java
+++ management/src/main/java/dk/kvalitetsit/itukt/management/service/ManagementService.java
@@ -1,7 +1,15 @@
 package dk.kvalitetsit.itukt.management.service;
 
 
-import dk.kvalitetsit.itukt.common.CRUD;
+import dk.kvalitetsit.itukt.common.exceptions.ServiceException;
+import dk.kvalitetsit.itukt.common.model.Clause;
 
-public interface ManagementService<T> extends CRUD<T> {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ManagementService {
+    Clause create(Clause entry) throws ServiceException;
+    Optional<Clause> read(UUID id) throws ServiceException;
+    List<Clause> readAll() throws ServiceException;
 }

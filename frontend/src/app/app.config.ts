b7388569d7456b73ad6ace69525a3c947a0cf6c4
Kristian Dorland <kdo@kvalitetsit.dk>
2025-09-19 12:55:01 +0200

    IUAKT-81: Environment til produktion

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app.routes';
+
+import { environment } from '../environments/environment';
+import { BASE_PATH } from '@api/variables';
+
+console.log('BASE_PATH (environment):', environment.apiBaseUrl);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       // tilføj eventuelt withInterceptors(...) senere...
-    )
+    ),
+    { provide: BASE_PATH, useValue: environment.apiBaseUrl }
   ]
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
+  production: false,
   apiBaseUrl: 'http://localhost:8080'
 };
-
-// TODO: Vi skal også have noget prod miljø-agtigt i en fil der hedder environment.prod.ts
-// TODO: .. hvor apiBaseUrl peger relativt, da vi deployer frontend og backend sammen

4bd559c3c11eef5b9809db98b050c381d035b09b
Thomas Lange <tsl@kvalitetsit.dk>
2025-10-02 09:37:42 +0200

    Don't insert duplicate skipped_validation entries

--- integrationtest/src/test/java/dk/kvalitetsit/itukt/integrationtest/repository/SkippedValidationRepositoryImplIT.java
+++ integrationtest/src/test/java/dk/kvalitetsit/itukt/integrationtest/repository/SkippedValidationRepositoryImplIT.java
@@ -51,4 +51,14 @@ class SkippedValidationRepositoryImplIT extends BaseTest {
         assertTrue(createdEntityExists, "Skipped validation should exist after creation");
         assertFalse(uncreatedEntityExists, "Skipped validation should not exist when not created");
     }
+
+    @Test
+    void create_SameEntityTwice_DoesNotFail() {
+        var condition = new ExpressionEntity.StringConditionEntity(Expression.Condition.Field.INDICATION, "test");
+        var clause = clauseRepository.create(new ClauseForCreation("test", condition));
+        var skippedValidation = new SkippedValidationEntity(clause.id(), "actor", "person");
+
+        skippedValidationRepository.create(List.of(skippedValidation));
+        assertDoesNotThrow(() -> skippedValidationRepository.create(List.of(skippedValidation)), "Creating the same skipped validation twice should not fail");
+    }
 }

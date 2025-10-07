1fc91ebd53db16220435b6f730990578956805fc
Henrik Eidnes <hen@kvalitetsit.dk>
2025-10-07 13:36:11 +0200

    Avoid inconvenience in historic drug medication validation
    If an ExistingDrugMedicationConditionExpression is inside an "or" expression, and it throws an 'ExistingDrugMedicationRequiredException', and the other side of the "or" expression validates, the entire validation will still fail.

--- common/src/main/java/dk/kvalitetsit/itukt/common/model/BinaryExpression.java
+++ common/src/main/java/dk/kvalitetsit/itukt/common/model/BinaryExpression.java
@@ -1,26 +1,23 @@
 package dk.kvalitetsit.itukt.common.model;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public record BinaryExpression(Expression left, Operator operator, Expression right) implements Expression {
 
     @Override
     public Optional<ValidationError> validates(ValidationInput validationInput) {
-        var l = left.validates(validationInput);
-        var r = right.validates(validationInput);
+        var leftError = left.validates(validationInput);
+        Supplier<Optional<ValidationError>> rightErrorLazy = () -> right.validates(validationInput); // Only validate right when necessary
         return switch (operator) {
-            case AND -> andError(l, r);
-            case OR -> orError(l, r);
+            case AND -> andError(leftError, rightErrorLazy.get());
+            case OR -> leftError.flatMap(l -> rightErrorLazy.get().map(r -> new OrError(l, r)));
         };
     }
 
-    private Optional<ValidationError> andError(Optional<ValidationError> left, Optional<ValidationError> right) {
-        Optional<ValidationError> bothError  = left.flatMap(l -> right.map(r -> new AndError(l, r)));
-        return bothError.or(() -> left.or(() -> right));
-    }
-
-    private Optional<ValidationError> orError(Optional<ValidationError> left, Optional<ValidationError> right) {
-        return left.flatMap(l -> right.map(r -> new OrError(l, r)));
+    private Optional<ValidationError> andError(Optional<ValidationError> leftError, Optional<ValidationError> rightError) {
+        Optional<ValidationError> bothError  = leftError.flatMap(l -> rightError.map(r -> new AndError(l, r)));
+        return bothError.or(() -> leftError.or(() -> rightError));
     }
 
     public enum Operator {OR, AND}

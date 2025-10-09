e5cd0150a8d06bcbe95b06c8af5d745383964da1
Jonas <jqh@kvalitetsit.dk>
2025-10-09 11:57:16 +0200

    removed not null validation

--- management/src/main/java/dk/kvalitetsit/itukt/management/service/model/ClauseForCreation.java
+++ management/src/main/java/dk/kvalitetsit/itukt/management/service/model/ClauseForCreation.java
@@ -7,8 +7,5 @@ public record ClauseForCreation(
         ExpressionEntity expression,
         String errorMessage
 ) {
-    public ClauseForCreation {
-        if (expression == null) throw new IllegalArgumentException("Expected an expression but was null");
-        if (name == null) throw new IllegalArgumentException("Expected a name but was null");
-    }
+
 }

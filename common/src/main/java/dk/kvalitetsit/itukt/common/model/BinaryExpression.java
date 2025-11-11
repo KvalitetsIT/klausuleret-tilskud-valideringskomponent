1cf7e2a4608ff520f888b2213baab2a4e7aeea23
Henrik Eidnes <hen@kvalitetsit.dk>
2025-11-11 15:07:49 +0100

    Refactor BinaryExpression

--- common/src/main/java/dk/kvalitetsit/itukt/common/model/BinaryExpression.java
+++ common/src/main/java/dk/kvalitetsit/itukt/common/model/BinaryExpression.java
@@ -3,42 +3,60 @@ package dk.kvalitetsit.itukt.common.model;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import static dk.kvalitetsit.itukt.common.model.BinaryExpression.OptionalValidationError.*;
 import static dk.kvalitetsit.itukt.common.model.ValidationError.*;
-
+import static java.util.Optional.*;
 
 public record BinaryExpression(Expression left, Operator operator, Expression right) implements Expression {
     public enum Operator {OR, AND}
 
     @Override
     public Optional<ValidationFailed> validates(ValidationInput validationInput) {
-        var leftError = left.validates(validationInput);
-        Supplier<Optional<ValidationFailed>> rightErrorLazy = () -> right.validates(validationInput); // Only validate right when necessary
+        var leftError = fromOptional(left.validates(validationInput));
+        Supplier<OptionalValidationError> rightError = () -> fromOptional(right.validates(validationInput));
         return switch (operator) {
-            case AND -> andError(leftError, rightErrorLazy);
-            case OR -> leftError.flatMap(l -> rightErrorLazy.get().map(r -> combineOr(l, r)));
+            case AND -> and(leftError, rightError.get());
+            case OR -> or(leftError, rightError);
         };
     }
 
-    static Optional<ValidationFailed> andError(Optional<ValidationFailed> optLeftFailure, Supplier<Optional<ValidationFailed>> rightLazy) {
-        Optional<ValidationFailed> bothErrorOrExistingMedicationMissing = optLeftFailure.flatMap(leftFailure -> switch (leftFailure) {
-            case ExistingDrugMedicationRequired ex -> Optional.of(ex);
-            case ValidationError leftError -> rightLazy.get().map(r -> switch (r) {
-                case ExistingDrugMedicationRequired ex -> ex;
-                case ValidationError rightError -> new AndError(leftError, rightError);
-            });
-        });
-        return bothErrorOrExistingMedicationMissing.or(() -> optLeftFailure.or(rightLazy));
-    }
-
-    record Pair(ValidationFailed vf1, ValidationFailed vf2) {} // To help pattern matching
-
-    static ValidationFailed combineOr(ValidationFailed left, ValidationFailed right) {
+    static Optional<ValidationFailed> and(OptionalValidationError left, OptionalValidationError right) {
         return switch (new Pair(left, right)) {
-            case Pair(ExistingDrugMedicationRequired l, var __) -> l;
-            case Pair(var __ , ExistingDrugMedicationRequired r) -> r;
-            case Pair(ValidationError l, ValidationError r) -> new OrError(l, r);
-            case Pair(ValidationError l, var __) -> l;
-            case Pair(var __, ValidationError r) -> r;
+            case Pair(Present(ExistingDrugMedicationRequired existingRequired), var __) -> of(existingRequired);
+            case Pair(var __, Present(ExistingDrugMedicationRequired existingRequired)) -> of(existingRequired);
+            case Pair(Present(ValidationError l), Present(ValidationError r)) -> of(new AndError(l, r));
+            case Pair(Present(ValidationError l), var __) -> of(l);
+            case Pair(var __, Present(ValidationError r)) -> of(r);
+            case Pair(Empty a, var __) -> empty();
+            case Pair(var __, Empty b) -> empty();
         };
     }
+
+    static Optional<ValidationFailed> or(OptionalValidationError left, Supplier<OptionalValidationError> right) {
+        if (left instanceof Empty)
+            return empty();
+        else
+            return switch (new Pair(left, right.get())) {
+                case Pair(Empty l, var __) -> empty();
+                case Pair(var __, Empty r) -> empty();
+                case Pair(Present(ValidationError l), Present(ValidationError r)) -> of(new OrError(l, r));
+                case Pair(Present(ExistingDrugMedicationRequired existingRequired), var __) -> of(existingRequired);
+                case Pair(var __, Present(ExistingDrugMedicationRequired existingRequired)) -> of(existingRequired);
+                case Pair(Present(ValidationError l), var __) -> empty(); // Only needed to satisfy the compiler, this case will never be hit
+                case Pair(var __, Present(ValidationError r)) -> empty(); // Only needed to satisfy the compiler, this case will never be hit
+            };
+    }
+
+    // Java cannot pattern match on Optional. This wrapper class allows it.
+    sealed interface OptionalValidationError permits Present, Empty {
+        record Present(ValidationFailed f) implements OptionalValidationError {}
+        record Empty() implements OptionalValidationError {}
+
+        static OptionalValidationError fromOptional(Optional<ValidationFailed> o) {
+            return o.<OptionalValidationError>map(Present::new).orElse(new Empty());
+        }
+    }
+
+    // To help pattern matching
+    record Pair(OptionalValidationError vf1, OptionalValidationError vf2) {}
 }
